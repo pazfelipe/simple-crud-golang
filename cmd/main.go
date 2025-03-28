c86e53749bc74f83fd65812060018a62d8fd5f01
Felipe Paz <xxfelipepaz@gmail.com>
2025-03-28 11:17:06 -0300

    feat: add product controller and products endpoint
    Introduce a new product controller to handle product-related logic and add a GET endpoint `/products` to retrieve product data. This change enhances the API by providing a new feature to fetch product information.

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,23 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"go-api/controller"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	server := gin.Default()
+
+	productController := controller.NewProductController()
+
 	server.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
+	server.GET("/products", productController.GetProducts)
 
 	server.Run(":8000")
 }
