18c5e72ba89ea38f6916806a42fb1bd52a225467
Felipe Paz <xxfelipepaz@gmail.com>
2025-03-28 11:19:59 -0300

    feat(product): add GetProducts method to ProductUseCase
    Introduce a new method to retrieve products from the ProductUseCase. This method returns an empty slice of products and a nil error, laying the groundwork for future implementation.

--- usecase/product.go
+++ usecase/product.go
@@ -1,8 +1,14 @@
 package usecase
 
+import "go-api/model"
+
 type ProductUseCase struct {
 }
 
 func NewProductUseCase() ProductUseCase {
 	return ProductUseCase{}
+}
+
+func (u *ProductUseCase) GetProducts() ([]model.Product, error) {
+	return []model.Product{}, nil
 }
