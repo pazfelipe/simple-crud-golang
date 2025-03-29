62ffa4384a8d6ce27cbd58e4579ee9dca0be69d1
Felipe Paz <xxfelipepaz@gmail.com>
2025-03-29 00:01:25 -0300

    feat(product): add CreateProduct endpoint to handle product creation
    This commit introduces a new endpoint `CreateProduct` in the product controller to handle the creation of products. It binds the incoming JSON request to a `model.Product` struct, validates the input, and uses the `productUseCase` to create the product in the database. If successful, it returns the created product with a 201 status code; otherwise, it returns appropriate error responses.

--- controller/product.go
+++ controller/product.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"go-api/model"
 	"go-api/usecase"
 	"net/http"
 
@@ -27,4 +28,27 @@ func (c *controller) GetProducts(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusOK, products)
+}
+
+func (c *controller) CreateProduct(ctx *gin.Context) {
+	var product model.Product
+	err := ctx.BindJSON(&product)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	productCreated, err := c.productUseCase.CreateProduct(product)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, productCreated)
 }
